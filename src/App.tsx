83871b9e0605b72eac3b57626ba89a8a1e6ebc02
devfilipealmeida <filipesilva504@gmail.com>
2024-04-16 20:21:45 -0300

    ultimas configurações de validação e transform do zod

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,21 @@ import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 const createUserFormSchema = z.object({
+  name: z.string()
+    .nonempty('O nome é obrigatório')
+    .transform(name => {
+      return name.trim().split(' ').map(word => {
+        return word[0].toLocaleUpperCase().concat(word.substring(1))
+      })
+    .join(' ')
+    }),
   email: z.string()
     .nonempty('O e-mail é obrigatório')
-    .email('Fromato de e-mail inválido'),
+    .email('Fromato de e-mail inválido')
+    .toLowerCase()
+    .refine(email => {
+      return email.endsWith('gmail.com')
+    }, 'O e-mail precisa ser do Gmail'),
   password: z.string()
     .min(6, 'A senha precisa de no mínimo 6 caracteres'),
 })
@@ -21,7 +33,7 @@ function App() {
     register, 
     handleSubmit, 
     formState: { errors } 
-  } = useForm({
+  } = useForm<CreateUserFormData>({
     resolver: zodResolver(createUserFormSchema),
   });
 
@@ -36,9 +48,19 @@ function App() {
         className='flex flex-col gap-4 w-full max-w-xs'
       >
         <div className='flex flex-col gap-1'>
-          <label htmlFor=''>E-mail</label>
+          <label htmlFor='name'>Nome</label>
           <input 
             type='text'
+            className='border border-zinc-800 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white'
+            {...register('name')}  
+          />
+          {errors.name && <span>{errors.name.message}</span>}	
+        </div>
+
+        <div className='flex flex-col gap-1'>
+          <label htmlFor='email'>E-mail</label>
+          <input 
+            type='email'
             className='border border-zinc-800 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white'
             {...register('email')}  
           />
@@ -46,7 +68,7 @@ function App() {
         </div>
 
         <div className='flex flex-col gap-1'>
-          <label htmlFor=''>Password</label>
+          <label htmlFor='password'>Password</label>
           <input 
             type='password'
             className='border border-zinc-800 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white'
